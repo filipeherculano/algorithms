1ea34c052ed2783091ca6b69418f62fd96907da4
Filipe Herculano <filipeherculanorocha@gmail.com>
2017-08-17 16:34:41 -0300

    Needleman Wunsch variants

---
+++ Needleman-Wunsch_Edit_Distance.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define maxn 1111111
+
+using namespace std;
+
+const double EPS = 1e-6;
+
+int n,m;
+string a,b;
+
+int main(){
+	cin >> a >> b;
+	n = a.size();
+	m = b.size();
+	int pd[n+1][m+1]; memset(pd,0x3f3f3f3f,sizeof pd);
+	pd[0][0] = 0;
+	for(int j = 1; j <= m; j++) pd[0][j] = j;
+	for(int i = 1; i <= n; i++) pd[i][0] = i;
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= m; j++){
+			pd[i][j] = min(pd[i][j], 1+pd[i-1][j]);
+			pd[i][j] = min(pd[i][j], 1+pd[i][j-1]);
+			pd[i][j] = min(pd[i][j],(a[i-1] == b[j-1] ? 0:1)+pd[i-1][j-1]);
+		} 
+	} 
+	cout << pd[n][m] << endl;
+	return 0;
+}
---
+++ Needleman-Wunsch_LCS.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define maxn 1111111
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f;
+const double EPS = 1e-6;
+
+int n,m;
+string a,b;
+
+int main(){
+	cin >> a >> b;
+	n = a.size();
+	m = b.size();
+	int pd[n+1][m+1];
+	memset(pd,0,sizeof pd);
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= m; j++){
+			pd[i][j] = max(pd[i][j], pd[i-1][j]);
+			pd[i][j] = max(pd[i][j], pd[i][j-1]);
+			pd[i][j] = max(pd[i][j],(a[i-1] == b[j-1] ? 1:-INF)+pd[i-1][j-1]);
+		} 
+	}
+	cout << pd[n][m] << endl;
+	return 0;
+}
