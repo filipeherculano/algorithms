d6198e4cdfc6b4069b235166eb50d5ffea60449d
Filipe Herculano <filipeherculanorocha@gmail.com>
2017-08-18 14:48:39 -0300

    upper to lower

--- LIS.cpp
+++ LIS.cpp
@@ -13,7 +13,7 @@ int main(){
 	scanf("%d",&n);
 	for(int i = 0; i < n; i++) scanf("%d",&v[i]);
 	for(int i = 0; i < n; i++){
-		int val = distance(answer.begin(), upper_bound(answer.begin(), answer.end(), v[i]));
+		int val = distance(answer.begin(), lower_bound(answer.begin(), answer.end(), v[i]));
 		if(val == answer.size()) answer.push_back(v[i]);
 		else answer[val] = v[i];
 	}
