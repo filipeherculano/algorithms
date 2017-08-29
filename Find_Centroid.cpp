d82410c50b42e0bee3b0cc0f9941dd4fc57eae1b
Filipe Herculano <filipeherculanorocha@gmail.com>
2017-08-29 00:17:41 -0300

    Changing find

--- Find_Centroid.cpp
+++ Find_Centroid.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 const double EPS = 1e-6;
 
-int n, centroid, maxComponent = maxn, Size[maxn], Parent[maxn];
+int n, Size[maxn], Parent[maxn];
 vector<int> adj[maxn];
 
 int dfs(int i){
@@ -19,20 +19,18 @@ int dfs(int i){
 	return Size[i] = res;
 }
 
-void Find_Centroid(){
-	for(int i = 0; i < n; i++){
-		int comp = 0;
-		for(int j = 0; j < adj[i].size(); j++){
-			if(adj[i][j] != Parent[i]){
-				comp = max(comp, Size[adj[i][j]]);
+int Find_Centroid(int i){
+	int next, sz = 0;
+	for(int j = 0; j < adj[i].size(); j++){
+		if(adj[i][j] != Parent[i]){
+			if(sz < Size[adj[i][j]]){
+				sz = Size[adj[i][j]];
+				next = adj[i][j];
 			}
 		}
-		comp = max(comp, Size[0]-Size[i]);
-		if(maxComponent > comp){
-			maxComponent = comp;
-			centroid = i;
-		}
 	}
+	if(sz > n/2) return Find_Centroid(next);
+	return i;
 }
 
 int main(){
@@ -40,13 +38,11 @@ int main(){
 	scanf("%d",&n);
 	for(int i = 0; i < n-1; i++){
 		int a,b;
-		scanf("%d %d",&a,&b);
+		scanf("%d %d",&a,&b); a--; b--;
 		adj[a].push_back(b);
 		adj[b].push_back(a);
 	}
 	dfs(0);
-	Find_Centroid();
-	printf("Centroid : %d\n", centroid);
-	printf("Max Component : %d\n", maxComponent);
+	printf("Centroid : %d\n", Find_Centroid(0)+1);
 	return 0;
 }
