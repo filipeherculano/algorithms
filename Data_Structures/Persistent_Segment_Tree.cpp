85fc1c6aef8b81e8031616315959eccf4487efab
Filipe Herculano <filipeherculanorocha@gmail.com>
2017-09-13 16:44:31 -0300

    Persistent segment tree

---
+++ Data_Structures/Persistent_Segment_Tree.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define maxn 2000000
+
+using namespace std;
+
+int n, next_node;
+int esq[30*maxn],dir[30*maxn],st[30*maxn],root[30*	maxn];
+
+int build(int L, int R){
+	int p = next_node++;
+	if(L == R) st[p] = 0;
+	else {
+		int mid = (L+R)/2;
+		esq[p] = build(L,mid);
+		dir[p] = build(mid+1,R);
+		st[p] = st[esq[p]] + st[dir[p]];
+	}
+	return p;
+}
+
+int update(int old, int L, int R, int pos, int v){
+	int p = next_node++;
+	if(L == R && L == pos){
+		st[p] = v;
+		return p;
+	}
+	int mid = (L+R)/2;
+	if(pos <= mid){
+		dir[p] = dir[old];
+		esq[p] = update(esq[old],L,mid,pos,v);
+	} else {
+		dir[p] = update(dir[old],mid+1,R,pos,v);
+		esq[p] = esq[old];
+	}
+	st[p] = st[esq[p]] + st[dir[p]];
+	return p;
+}
+
+int query(int p, int L, int R, int i, int j){
+	if(L > j || R < i) return 0;
+	if(i <= L && R <= j) return st[p];
+	int mid = (L+R)/2;
+	int p1 = query(esq[p],L,mid,i,j);
+	int p2 = query(dir[p],mid+1,R,i,j);
+	return p1+p2;
+}
+
+int main(){
+	return 0;
+}
