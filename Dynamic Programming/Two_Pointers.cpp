c1fb5bb3f1eb928e9958dc30f3731227a892b980
Filipe Herculano <filipeherculanorocha@gmail.com>
2018-08-20 18:43:47 -0300

    Two Pointers algo

--- Dynamic Programming/Two_Pointers.cpp
+++ Dynamic Programming/Two_Pointers.cpp
@@ -1,6 +1,6 @@
 /**
  * @brief This program is an example of the Two Pointer technique,
- * used to solve Subarray Sum problem in O(n) 
+ * used to solve Subarray Sum problem in O(n) time complexity
  * 
  * @file Two_Pointers.cpp
  * @author Filipe Herculano Rocha
@@ -13,17 +13,22 @@ using namespace std;
 
 int n, target, arr[maxn];
 
-int maint(){
+int main(){
     cin >> n >> target;
     for(int i = 0; i < n; i++) scanf("%d",&arr[i]);
     int l = 0, r = 0, current = 0;
-    while(l < n){
-        if(r < n){
-            
-        }
-        if(r < n && current + arr[r] > target){
-            
-        }
+    bool found = false;
+    while(r < n && !found){
+        if(current + arr[r] > target){
+            l++;
+            r = max(l,r);
+        } else if(current + arr[r] < target){
+            current += arr[r];
+            r++;
+        } else found = true;
     }
+
+    if(found) puts("Yes");
+    else puts("No");
     return 0;     
 }
