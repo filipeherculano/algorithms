abb4a1fbfe9592cf165c182313b890d0ea4717cc
filipeherculano <filipe.herculano@lds.ifce.edu.br>
2018-09-11 21:16:32 -0300

    Adding new graph algorithms

--- Graph/MST_Prim.cpp
+++ Graph/MST_Prim.cpp
@@ -10,21 +10,22 @@ using namespace std;
 
 int mst;
 vector<int> adj[maxn], wadj[maxn];
-priority_queue< pair<int,int> > pq;
+priority_queue<pair<int, int> > pq;
 bool vis[maxn];
 
 void process(int u){
     vis[u] = true;
-    for(int j = 0; j < adj[u].size(); j++)
-        if(!vis[adj[u][j]]) 
-            pq.push({-wadj[u][j],adj[u][j]});
+    for (int j = 0; j < adj[u].size(); j++)
+        if (!vis[adj[u][j]])
+            pq.push({ -wadj[u][j], adj[u][j] });
 }
 
 void Prim(){
     process(0);
-    while(!pq.empty()) {
+    while (!pq.empty()) {
         int u = pq.top().second, w = -pq.top().first;
         pq.pop();
-        if(!vis[u]) mst += w, process(u);
+        if (!vis[u])
+            mst += w, process(u);
     }
 }
